fc5f0fc138c3bd4c1cef9132190fb0147f9bf844
ToniSkay <59788580+ToniSkay@users.noreply.github.com>
2020-01-30 23:06:55 +0300

    Добавили переход на стр логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
-
+from .pages.locator import MainPageLocators
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+    link = MainPageLocators.LINK
+    login_page = LoginPage(browser, link)
     page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    page.open() #открываем браузер с ссылкой указанной в константе
+    page.go_to_link_page() #переходим на страницу логина
+    login_page.should_be_login_page()
